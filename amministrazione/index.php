944aff139fd45efb4d8864cc5951bd2dd2b931e8
Jacopo Beragnoli <beragnoli.jacopo@studenti-ittfedifermi.edu.it>
2021-03-27 10:45:23 +0100

    Indentazione Aggiustata indentazione files

--- amministrazione/index.php
+++ amministrazione/index.php
@@ -21,33 +21,33 @@
 		height: 400px;
 	}
 	</style>
-<div class="flex items-center">
-	<a class="w-full" onclick="mostraSidebar()">
-		<svg xmlns="http://www.w3.org/2000/svg" width="52" height="52" fill="currentColor" class="bi bi-list cursor-pointer" viewBox="0 0 16 16">
-			<path class="ml-8" fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-		</svg>
-	</a>
-	<div class="flex w-1/3 justify-self-end items-center ml-64">
-		<?php
-		if(isset($_SESSION['username'])) {
-			echo 'Bentornato, ' . $_SESSION['username'] . '!';
-			?>
-			<form action="" method="POST">
-				<button class="ml-2 focus:outline-none text-purple-600 text-sm py-2.5 px-5 rounded-md hover:bg-purple-100" type="submit">logout</button>
-			</form>
+	<div class="flex items-center">
+		<a class="w-full" onclick="mostraSidebar()">
+			<svg xmlns="http://www.w3.org/2000/svg" width="52" height="52" fill="currentColor" class="bi bi-list cursor-pointer" viewBox="0 0 16 16">
+				<path class="ml-8" fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+			</svg>
+		</a>
+		<div class="flex w-1/3 justify-self-end items-center ml-64">
 			<?php
-			if($_SERVER['REQUEST_METHOD'] == 'POST') {
-				unset($_SESSION['username']);
+			if(isset($_SESSION['username'])) {
+				echo 'Bentornato, ' . $_SESSION['username'] . '!';
 				?>
-				<script>
-				location.reload();
-			</script>
-			<?php
-		}
-	}
-	?>
+				<form action="" method="POST">
+					<button class="ml-2 focus:outline-none text-purple-600 text-sm py-2.5 px-5 rounded-md hover:bg-purple-100" type="submit">logout</button>
+				</form>
+				<?php
+				if($_SERVER['REQUEST_METHOD'] == 'POST') {
+					unset($_SESSION['username']);
+					?>
+					<script>
+					location.reload();
+					</script>
+					<?php
+				}
+			}
+			?>
+		</div>
 	</div>
-</div>
 
 	<div class="md:flex flex-col md:flex-row md:min-h-screen w-full" id="sidebar">
 		<nav :class="{'block': open, 'hidden': !open}" class="flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">
--- amministrazione/pietanza/inserimento.php
+++ amministrazione/pietanza/inserimento.php
@@ -20,33 +20,33 @@
   <script src="../../static/js/jquery.js"></script>
 </head>
 <body>
-<div class="flex items-center">
-  <a class="w-full" onclick="mostraSidebar()">
-    <svg xmlns="http://www.w3.org/2000/svg" width="52" height="52" fill="currentColor" class="bi bi-list cursor-pointer" viewBox="0 0 16 16">
-      <path class="ml-8" fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-    </svg>
-  </a>
-  <div class="flex w-1/3 justify-self-end items-center ml-64">
-    <?php
-    if(isset($_SESSION['username'])) {
-      echo 'Bentornato, ' . $_SESSION['username'] . '!';
-      ?>
-      <form action="" method="POST">
-        <button class="ml-2 focus:outline-none text-purple-600 text-sm py-2.5 px-5 rounded-md hover:bg-purple-100" type="submit">logout</button>
-      </form>
+  <div class="flex items-center">
+    <a class="w-full" onclick="mostraSidebar()">
+      <svg xmlns="http://www.w3.org/2000/svg" width="52" height="52" fill="currentColor" class="bi bi-list cursor-pointer" viewBox="0 0 16 16">
+        <path class="ml-8" fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+      </svg>
+    </a>
+    <div class="flex w-1/3 justify-self-end items-center ml-64">
       <?php
-      if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        unset($_SESSION['username']);
+      if(isset($_SESSION['username'])) {
+        echo 'Bentornato, ' . $_SESSION['username'] . '!';
         ?>
-        <script>
-        location.reload();
-        </script>
+        <form action="" method="POST">
+          <button class="ml-2 focus:outline-none text-purple-600 text-sm py-2.5 px-5 rounded-md hover:bg-purple-100" type="submit">logout</button>
+        </form>
         <?php
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+          unset($_SESSION['username']);
+          ?>
+          <script>
+          location.reload();
+          </script>
+          <?php
+        }
       }
-    }
-    ?>
+      ?>
+    </div>
   </div>
-</div>
   <div class="md:flex flex-col md:flex-row md:min-h-screen w-full" id="sidebar">
     <nav :class="{'block': open, 'hidden': !open}" class="flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">
       <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="../../">Home</a>
